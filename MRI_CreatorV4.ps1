aa7bdc528eb95952be3c95710e277025b3eea5be
JStone21 <49456340+JStone21@users.noreply.github.com>
2023-12-03 20:11:06 -0500

    Added FolderBrowser and Role Checking
    Script now displays the Windows FolderBrowserDialog instead of asking the user to enter the full path, which is error prone. Script now also checks for administrative privileges required be Clean-Disk and New-Partition cmdlets.

---
+++ MRI_CreatorV4.ps1
@@ -0,0 +1,169 @@
+﻿#----------VARIABLES----------
+$IsDriveAvailable = $false
+$RemovableDriveList = [System.Collections.ArrayList]::new()
+$MRIFilePath = ""
+$MRIPathIsCorrect = $false
+
+
+
+#----------FUNCTIONS----------
+#This function checks if the script is running as an administrator
+function Check-IsElevated{
+    
+    #Get current ID of active user
+    $id = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+
+    #Get the role of the active user
+    $p = New-Object System.Security.Principal.WindowsPrincipal($id)
+
+    #If the role of the user is admin, output true. Otherwise output false.
+    if ($p.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)){
+        Write-Output($true)
+    }
+    else{
+        Write-Output($false)
+    }
+}
+
+function Format-Drives{
+	foreach($Drive in $RemovableDriveList){
+		Write-Host("Formatting drive: ", $Drive.DiskNumber," ",$Drive.FriendlyName,"`n")
+		
+		
+			Write-Host("Clearing drive: ", $Drive.DiskNumber)
+		#Need to set -confirm to $false for auto format
+			Clear-Disk -Number $Drive.DiskNumber -RemoveData -confirm:$true
+		
+
+		$DiskInfo = Get-Disk -Number $Drive.DiskNumber
+		if ($DiskInfo.size -gt 32000000000){
+			Write-Host("Drive ", $Drive.DiskNumber, " is larger than 32 GB. Using Only 32GB of space for FAT32.")
+		
+		
+		
+		
+			New-Partition $Drive.DiskNumber -AssignDriveLetter -IsActive -Size 32000000000
+		}
+		
+		else{
+			Write-Host("Drive ",$Drive.DiskNumber," smaller than or equal to 32GB. Using full drive space.")
+			
+		
+			New-Partition $Drive.DiskNumber -AssignDriveLetter -IsActive -UseMaximumSize
+		}
+		
+		$PartitionInfo = Get-Partition -DiskNumber $Drive.DiskNumber
+		Write-Host("Formatting Drive",$PartitionInfo.DriveLetter, "to FAT32")
+		
+		Format-Volume -DriveLetter $PartitionInfo.DriveLetter -FileSystem FAT32 -NewFileSystemLabel "MRI" -Force
+	}
+}
+
+function Create-MRI{
+	foreach($Drive in $RemovableDriveList){
+		$PartitionInfo = Get-Partition -DiskNumber $Drive.DiskNumber
+		
+		#Append \* for copy-item cmdlet to copy all items in folder
+		$ItemsToCopy = $MRIFilePath+'\*'
+		
+		#Append :\ as $PartitionInfo.DriveLetter only lists a character, not a path
+		$DestinationPath = $PartitionInfo.DriveLetter+':\'
+		
+		Write-Host("Writing MRI Files to drive: ", $PartitionInfo.DriveLetter," ", $Drive.FriendlyName,"`n")
+		try{
+			copy-item -path $ItemsToCopy -destination $DestinationPath -Recurse
+		}
+		catch{
+			Write-Host("Could not copy files to drive ", $PartitionInfo.DriveLetter)
+			Write-Host($_)
+		}
+	}
+}
+
+#This function opens the windows folder brower and outputs a selected folder
+function Get-MRIFilePath{
+    
+    write-host("What is the MRI file path?")
+    
+    $openFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    
+    $openFolderDialog.ShowDialog()
+   
+    $openFolderDialog.SelectedPath
+}
+
+
+#----------MAIN----------
+#If the active user is not running the script as an administrator, stop execution.
+if(-not(Check-IsElevated)){
+    throw "Please run this script as an Administrator"
+}
+
+
+#Obtains a list of drives used for file storage.
+$RemovableDriveList = Get-Disk | Where-Object -FilterScript {$_.Bustype -Eq "USB"}
+
+#If there are no removable drives, notify the user.
+if($RemovableDriveList.count -eq 0){
+	
+	Write-Host("There are no drives available for MRI Creation.")
+}
+
+#If there are removable drives detected, prompt user for MRI and Customizer locations, then list all available drives.
+else{
+	$IsDriveAvailable = $true
+	
+	#Ask User for file path to MRI files to copy
+		while($MRIPathIsCorrect -ne $true){
+	
+		$MRIFilePath = Get-MRIFilePath
+	
+		Write-Host("MRI file path is: ", $MRIFilePath,"`n")
+	
+		$MRIPathIsCorrect = Read-Host("Is the MRI file path correct? y/n")
+			if(($MRIPathIsCorrect -eq "y") -or ($MRIPathIsCorrect -eq "yes")){
+			$MRIPathIsCorrect = $true
+			}
+		}
+	#Empty line for spacing
+	Write-Host("")
+	
+	Write-Host("The following drives are available for MRI Creation:")
+	
+	#Print RemovableDriveList
+	$RemovableDriveList
+	}
+	
+	
+#If drives are available to format, prompt user for confirmation that they want to format ALL drives
+if ($IsDriveAvailable -eq $true){
+		$DrivesToFormat = Read-Host("Would you like to convert all of these drives to MRI? y/n")
+		
+		if(($DrivesToFormat -eq "y") -or ($DrivesToFormat -eq "yes")){
+			$UserConfirm = Read-Host("WARNING: THIS WILL WIPE ALL INFORMATION FROM THESE DRIVES. ARE YOU SURE? y/n")
+		}
+		
+		if(($UserConfirm -eq "y") -or ($UserConfirm -eq "yes")){
+			Write-Host("Formatting!`n")
+			try{
+				Format-Drives($RemovableDriveList) -ErrorAction exit
+			}
+			
+			catch{
+					Write-Host("Error: Could not format the disks.")
+					Write-Host($_)
+			}
+			
+			Create-MRI($RemovableDriveList)
+			Write-host("MRI Creation complete.")
+		}
+		#If the user enter anything other than "y" or "yes", do not format drives.
+		else{
+			Write-Host("Aborting. Drives have not been formatted.")
+		}
+	}
+	
+
+
+
+
